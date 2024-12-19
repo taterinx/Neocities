46e4bfa1decc4274ff84453df6b75cdccaa5a33d
taterinx <37455494+taterinx@users.noreply.github.com>
2024-12-19 20:27:48 +0000

    Setting up some secret stuff to maybe forget about LOL

---
+++ public/projects/comic/js/comic_footer.js
@@ -0,0 +1,7 @@
+document.querySelector(".writeFooter").innerHTML = `
+    <footer align="center">
+        <p>Title © Fletcher Fraher and Victoria Bramham</p>
+        <p><strong>Powered by</strong></p>
+        <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a>
+    </footer>
+`;
